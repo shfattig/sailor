06e4720d938f42b3c2361cb2243bfc4f3bae7523
Shaun Fattig <shaun@fattig.net>
2025-03-21 00:06:29 -0500

    integrate taskchampion.rs backend, retrieve task ids from frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,87 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use std::path::PathBuf;
+
+use serde::{Deserialize, Serialize};
+use taskchampion::chrono::Utc;
+use taskchampion::storage::AccessMode;
+use taskchampion::{Operations, Replica, Status, StorageConfig, Uuid};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    id: String,
+    content: String,
+    due_date: Option<String>,
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_all_tasks() -> Result<Vec<String>, String> {
+    // Return Result type
+    // Create a new Replica, storing data on disk.
+    let taskdb_dir = PathBuf::from(".tasks");
+    let storage = StorageConfig::OnDisk {
+        taskdb_dir,
+        create_if_missing: true,
+        access_mode: AccessMode::ReadWrite,
+    }
+    .into_storage()
+    .map_err(|e| e.to_string())?; // Convert error to string
+    println!("Storage created!");
+
+    let mut replica = Replica::new(storage);
+    let uuid = Uuid::new_v4();
+    let mut ops = Operations::new();
+    let mut t = replica
+        .create_task(uuid, &mut ops)
+        .map_err(|e| e.to_string())?;
+    t.set_description("my first task".into(), &mut ops)
+        .map_err(|e| e.to_string())?;
+    t.set_status(Status::Pending, &mut ops)
+        .map_err(|e| e.to_string())?;
+    t.set_entry(Some(Utc::now()), &mut ops)
+        .map_err(|e| e.to_string())?;
+
+    replica.commit_operations(ops).map_err(|e| e.to_string())?;
+    println!("Task created!");
+
+    // // Set up a local, on-disk server.
+    // let server_config = ServerConfig::Local { server_dir };
+    // let mut server = server_config.into_server()?;
+
+    // // Sync to that server.
+    // replica.sync(&mut server, true)?;
+
+    // vec![
+    //     Task {
+    //         id: "1".to_string(),
+    //         content: "Task 1".to_string(),
+    //         due_date: Some("2025-10-04".to_string()),
+    //     },
+    //     Task {
+    //         id: "2".to_string(),
+    //         content: "Task 2".to_string(),
+    //         due_date: Some("2025-10-05".to_string()),
+    //     },
+    // ]
+
+    let task_uuids = replica.all_task_uuids().map_err(|e| e.to_string())?;
+
+    Ok(task_uuids
+        .into_iter()
+        .map(|uuid| uuid.to_string())
+        .collect())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_all_tasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
