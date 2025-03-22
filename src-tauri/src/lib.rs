e0ff8f9c00669bb0dd193ba7a33a76d5c383dbaa
Shaun Fattig <shaun@fattig.net>
2025-03-22 05:53:13 -0500

    add create_tasks method

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -53,14 +53,15 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn get_all_tasks() -> Result<Vec<String>, String> {
+fn create_task(description: &str) -> Result<String, String> {
     with_replica(|replica| {
         let uuid = Uuid::new_v4();
         let mut ops = Operations::new();
+
         let mut t = replica
             .create_task(uuid, &mut ops)
             .map_err(|e| e.to_string())?;
-        t.set_description("my first task".into(), &mut ops)
+        t.set_description(description.into(), &mut ops)
             .map_err(|e| e.to_string())?;
         t.set_status(Status::Pending, &mut ops)
             .map_err(|e| e.to_string())?;
@@ -68,30 +69,15 @@ fn get_all_tasks() -> Result<Vec<String>, String> {
             .map_err(|e| e.to_string())?;
 
         replica.commit_operations(ops).map_err(|e| e.to_string())?;
-        println!("Task created!");
 
-        // // Set up a local, on-disk server.
-        // let server_config = ServerConfig::Local { server_dir };
-        // let mut server = server_config.into_server()?;
+        Ok(uuid.to_string())
+    })
+}
 
-        // // Sync to that server.
-        // replica.sync(&mut server, true)?;
-
-        // vec![
-        //     Task {
-        //         id: "1".to_string(),
-        //         content: "Task 1".to_string(),
-        //         due_date: Some("2025-10-04".to_string()),
-        //     },
-        //     Task {
-        //         id: "2".to_string(),
-        //         content: "Task 2".to_string(),
-        //         due_date: Some("2025-10-05".to_string()),
-        //     },
-        // ]
-
+#[tauri::command]
+fn get_all_tasks() -> Result<Vec<String>, String> {
+    with_replica(|replica| {
         let task_uuids = replica.all_task_uuids().map_err(|e| e.to_string())?;
-
         Ok(task_uuids
             .into_iter()
             .map(|uuid| uuid.to_string())
@@ -103,7 +89,7 @@ fn get_all_tasks() -> Result<Vec<String>, String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, get_all_tasks])
+        .invoke_handler(tauri::generate_handler![greet, get_all_tasks, create_task])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
